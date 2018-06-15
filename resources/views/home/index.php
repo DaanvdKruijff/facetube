1484597a7868e3be1ee7398c231b1c4b7725f8eb
TheoC99 <36277618+TheoC99@users.noreply.github.com>
2018-06-15 09:26:22 +0200

    Login/registratiepagina's

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -1,7 +1,8 @@
 <?php $app->render('shared/header'); ?>
 
 <div class="container">
-    Hello World!
+    <button type="button" name="Registeren">Registeren</button>
+    <button type="button" name="Login">Login</button>
 </div>
 
 <?php $app->render('shared/footer'); ?>
