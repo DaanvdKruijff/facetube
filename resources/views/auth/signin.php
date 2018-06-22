c2d8982b58dbf692d34ddddce5d0ccbde7bf5ae2
Maarten Buis <buismaarten@outlook.com>
2018-06-22 09:02:13 +0200

    Added authentication forms

--- resources/views/auth/signin.php
+++ resources/views/auth/signin.php
@@ -1,7 +1,19 @@
 <?php $app->render('shared/header'); ?>
 
 <div class="container">
-    Inloggen
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <div class="form-group">
+            <label>E-mailadres</label>
+            <input type="email" name="email" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label>Wachtwoord</label>
+            <input type="password" name="password" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
 </div>
 
 <?php $app->render('shared/footer'); ?>
--- resources/views/auth/signup.php
+++ resources/views/auth/signup.php
@@ -1,7 +1,29 @@
 <?php $app->render('shared/header'); ?>
 
 <div class="container">
-    Registreren
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <div class="form-group">
+            <label>Voornaam</label>
+            <input type="text" name="firstname" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label>Achternaam</label>
+            <input type="text" name="lastname" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label>E-mailadres</label>
+            <input type="email" name="email" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label>Wachtwoord</label>
+            <input type="password" name="password" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Sign up</button>
+    </form>
 </div>
 
 <?php $app->render('shared/footer'); ?>
