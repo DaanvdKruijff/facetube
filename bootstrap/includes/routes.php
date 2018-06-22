bf607a2d4178aa0e6a6718032c99a0346989faa7
DaanvdKruijff <39614833+DaanvdKruijff@users.noreply.github.com>
2018-06-22 09:53:07 +0200

    routers

--- bootstrap/includes/routes.php
+++ bootstrap/includes/routes.php
@@ -9,3 +9,4 @@ $app->route('/auth/signin', [$controllers['AuthController'], 'signin']);
 
 // Post
 $app->route('/post/create', [$controllers['PostsController'], 'create']);
+$app->route('/post/view', [$controllers['PostsController'], 'view']);
