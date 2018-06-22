c51f424934bb79ffdf56d8a799cddc598434c983
Maarten Buis <buismaarten@outlook.com>
2018-06-22 10:01:39 +0200

    Changes

--- bootstrap/includes/routes.php
+++ bootstrap/includes/routes.php
@@ -9,11 +9,6 @@ $app->route('/auth/signin', [$controllers['AuthController'], 'signin']);
 // Dashboard
 $app->route('/dashboard', [$controllers['DashboardController'], 'index']);
 
-<<<<<<< HEAD
 // Posts
-$app->route('/post/create', [$controllers['PostsController'], 'create']);
-=======
-// Post
-$app->route('/post/create', [$controllers['PostsController'], 'create']);
-$app->route('/post/view', [$controllers['PostsController'], 'view']);
->>>>>>> 1837b993e5f487439223e234597ef129f80750c8
+$app->route('/posts/create', [$controllers['PostsController'], 'create']);
+$app->route('/posts/view', [$controllers['PostsController'], 'view']);
