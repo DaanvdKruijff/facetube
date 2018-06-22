4e15a91ed9e380066bf1b8a4469e3116c59c2f3b
DaanvdKruijff <39614833+DaanvdKruijff@users.noreply.github.com>
2018-06-22 10:05:15 +0200

    opmaak create

--- resources/views/posts/create.php
+++ resources/views/posts/create.php
@@ -1,6 +1,7 @@
 <?php $app->render('shared/header'); ?>
 
 <div class="container">
+	<h1>Maak uw bericht</h1>
 	<form name="PostMaken" method="post" target="">
 		<label>Titel</label><br>
 			<input type="text" name="titel"><br>
@@ -9,7 +10,7 @@
 			<textarea name="inhoud"></textarea><br>
 		
 		<label>Video</label><br>
-			<input type="file" name="video"><br>
+			<input type="file" name="video"><br><br>
 		
 		<input type="submit" value="Plaatsen" name="submit">
 	</form>
