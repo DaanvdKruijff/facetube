a4bee48d385c3184b41d9cf5f0029f0f6ae70545
Maarten Buis <buismaarten@outlook.com>
2018-06-29 09:04:08 +0200

    Added test for homepage

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -1,7 +1,19 @@
 <?php $app->render('shared/header'); ?>
 
 <div class="container">
-    <!--  -->
+    <div class="row">
+        <div class="col-sm-4">
+            <?php echo print_r($posts, true); ?>
+        </div>
+
+        <div class="col-sm-4">
+            <?php echo print_r($videos, true); ?>
+        </div>
+
+        <div class="col-sm-4">
+            <?php echo print_r($comments, true); ?>
+        </div>
+    </div>
 </div>
 
 <?php $app->render('shared/footer'); ?>
