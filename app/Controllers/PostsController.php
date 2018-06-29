914ad1c8d7b94feebdc54ca5d616668f8b6c8eb7
DaanvdKruijff <39614833+DaanvdKruijff@users.noreply.github.com>
2018-06-29 09:12:09 +0200

    Insert query's

--- app/Controllers/PostsController.php
+++ app/Controllers/PostsController.php
@@ -13,7 +13,15 @@ class PostsController extends Controller
 
         	if (isset($_POST["titel"], $_POST["inhoud"], $_POST["submit"])) {
 
-        		
+        		$query = $db->prepare('INSERT INTO Posts (GebruikersID, Titel, Inhoud) VALUES (?, ?, ?)');
+                $query->execute([SESSION['user']], [$_POST['titel'], [$_POST['inhoud']]);
+
+                if(isset($_POST['video']))
+                {
+                    $lastID = $db->insert_id;
+                    $query = $db->prepare('INSERT INTO Videos (PostID, Titel) VALUES (?, ?)');
+                    $query->execute([$lastID], [$_POST['titel']]);
+                }
         		
 			} 
 
