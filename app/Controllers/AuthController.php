98be5940260b7341e7e242f510a94d995ecf0f39
Maarten Buis <buismaarten@outlook.com>
2018-06-29 08:38:23 +0200

    Added user exists check by registration

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -14,11 +14,18 @@ class AuthController extends Controller
         $this->redirectIfAuthenticated();
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $query = $db->prepare('INSERT INTO Gebruikers (GebruikerID, Gebruikersnaam, Voornaam, Achternaam, Email, Wachtwoord) VALUES (NULL, ?, ?, ?, ?, ?)');
-            $query->execute([$_POST['username'], $_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['password']]);
+            $query = $db->prepare('SELECT GebruikerID FROM Gebruikers WHERE Email = ? AND Wachtwoord = ?');
+            $query->execute([$_POST['email'], $_POST['password']]);
 
-            // Check of gebruiker bestaat
-            // $this->app->view()->set('error', 'Deze gebruiker bestaat al.');
+            // Check if user is found
+            $found = $query->rowCount();
+
+            if (!$found) {
+                $query = $db->prepare('INSERT INTO Gebruikers (GebruikerID, Gebruikersnaam, Voornaam, Achternaam, Email, Wachtwoord) VALUES (NULL, ?, ?, ?, ?, ?)');
+                $query->execute([$_POST['username'], $_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['password']]);
+            } else {
+                $this->app->view()->set('error', 'Deze gebruiker bestaat al.');
+            }
 
             // Redirect
             $this->app->redirect('/auth/signin');
