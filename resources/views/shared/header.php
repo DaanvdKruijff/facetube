a7b03fd0f4ec8916bc57c52a4554fa6891ffa4c5
TheoC99 <36277618+TheoC99@users.noreply.github.com>
2018-07-06 12:06:28 +0200

    navbar aanpassing #3

--- resources/views/shared/header.php
+++ resources/views/shared/header.php
@@ -21,7 +21,7 @@
     <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
         <div class= "container">
           <a class="navbar-brand" href="/">
-            <img src="/assets/images/Facetube.png" width="150" height="65" alt="FaceTube">
+            <img src="/assets/images/Facetube.png" width="110" height="50" alt="FaceTube">
           </a>
               <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Zoek hier naar posts" aria-label="Search">
