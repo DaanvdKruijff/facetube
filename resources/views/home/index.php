4f1b08c31db79da97110da59dd24a7913013a979
Maarten Buis <buismaarten@outlook.com>
2018-07-06 09:14:47 +0200

    Homepage test

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -6,7 +6,7 @@
             <h2>Berichten</h2>
 
             <ul style="margin:0; padding:0;">
-                <?php foreach ($posts as $post): ?>
+                <?php print_r($posts); foreach ($posts as $post): ?>
                     <li style="list-style-type:none;">
                         <strong><?php echo htmlspecialchars($post['Titel']); ?></strong>
                         <br />
@@ -21,7 +21,7 @@
             <h2>Video's</h2>
 
             <ul style="margin:0; padding:0;">
-                <?php foreach ($videos as $video): ?>
+                <?php print_r($videos); foreach ($videos as $video): ?>
                     <li style="list-style-type:none;">
                         <strong><?php echo htmlspecialchars($video['Titel']); ?></strong>
                         <br />
@@ -36,7 +36,7 @@
             <h2>Reacties</h2>
 
             <ul style="margin:0; padding:0;">
-                <?php foreach ($comments as $comment): ?>
+                <?php print_r($comments); foreach ($comments as $comment): ?>
                     <li style="list-style-type:none;">
                         <strong>Gebruiker: <?php echo htmlspecialchars($comment['GebruikerID']); ?></strong>
                         <br />
