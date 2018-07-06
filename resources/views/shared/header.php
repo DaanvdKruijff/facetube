3073545ed79e50134b6a7947e6e268121144ea1d
TheoC99 <36277618+TheoC99@users.noreply.github.com>
2018-07-06 10:04:11 +0200

    fixed header

--- resources/views/shared/header.php
+++ resources/views/shared/header.php
@@ -18,10 +18,13 @@
         crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar sticky-top navbar-light bg-light">
       <a class="navbar-brand" href="/">
         <img src="/assets/images/FaceTube.png" width="110" height="50" class="d-inline-block align-top" alt="">
       </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
