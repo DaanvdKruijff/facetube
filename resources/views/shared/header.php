64ed8bf2df42eee2e4d62939a83f61f44516f46d
TheoC99 <36277618+TheoC99@users.noreply.github.com>
2018-07-06 11:31:19 +0200

    fix

--- resources/views/shared/header.php
+++ resources/views/shared/header.php
@@ -22,10 +22,10 @@
       <a class="navbar-brand" href="/">
         <img src="/assets/images/FaceTube.png" width="110" height="50" class="d-inline-block align-top" alt="">
       </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarText">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="/auth/signin">Inloggen</a>
