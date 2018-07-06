7dddf30f23182724ed224be38c663b8a69e3272d
TheoC99 <36277618+TheoC99@users.noreply.github.com>
2018-07-06 12:03:59 +0200

    navbar aanpassing #2

--- resources/views/shared/header.php
+++ resources/views/shared/header.php
@@ -21,11 +21,11 @@
     <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
         <div class= "container">
           <a class="navbar-brand" href="/">
-            <img src="/assets/images/Facetube.png" width="110" height="50" alt="FaceTube">
+            <img src="/assets/images/Facetube.png" width="150" height="65" alt="FaceTube">
           </a>
               <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Zoek hier naar posts" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken...</button>
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Zoeken...</button>
             </form>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
