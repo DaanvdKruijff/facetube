ced6fa52d866e96b93d8925b1bac555833d94618
TheoC99 <36277618+TheoC99@users.noreply.github.com>
2018-07-06 11:55:04 +0200

    zoekbalktest1

--- resources/views/shared/header.php
+++ resources/views/shared/header.php
@@ -23,6 +23,10 @@
           <a class="navbar-brand" href="/">
             <img src="/assets/images/Facetube.png" width="110" height="50" alt="FaceTube">
           </a>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken...</button>
+            </form>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
